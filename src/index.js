7c431752cc0e0e84b04c8e7c744822582ba1b31e
Sagar <sagar.jhaa@gmail.com>
2020-12-06 02:46:18 -0800

    read envs

--- src/index.js
+++ src/index.js
@@ -7,10 +7,18 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { GITHUB_SHA, GITHUB_EVENT_PATH, GITHUB_TOKEN, GITHUB_WORKSPACE, GITHUB_ACTOR } = process.env;
 
 // action file information: https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/metadata-syntax-for-github-actions
 async function main() {
   try {
+
+    console.log(GITHUB_SHA)
+    console.log(GITHUB_EVENT_PATH)
+    console.log(GITHUB_TOKEN)
+    console.log(GITHUB_WORKSPACE)
+    console.log(GITHUB_ACTOR)
+
 
     const github_token = core.getInput("GITHUB_TOKEN", { required: true })
     console.log(github_token);
