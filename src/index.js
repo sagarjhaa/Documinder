7e006352f7c8848661a9c7a6eaa75bfd4f47747c
Sagar <sagar.jhaa@gmail.com>
2020-12-05 22:16:10 -0800

    add core to get input values

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@
 // slack message to intimate if we need an update
 // mail if we need to alert
 
+const core = require('@actions/core')
 
 // action file information: https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/metadata-syntax-for-github-actions
 function main() {
-  console.log("documinder is live");
+  try {
+    const pr = core.getInput("pr", { required: true })
+    const rules = core.getInput("rules", { required: true })
+
+    console.log(`received pr - ${pr} and rules - ${rules}`)
+
+  } catch (error) {
+    console.log(`error occured ${error}`);
+  }
 }
 
 main();
