8b05daffc00e3812aefa7cebc98723d55773a4c7
Sagar <sagar.jhaa@gmail.com>
2020-12-06 02:49:16 -0800

    use the process envs

--- src/index.js
+++ src/index.js
@@ -7,24 +7,16 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { GITHUB_SHA, GITHUB_EVENT_PATH, GITHUB_TOKEN, GITHUB_WORKSPACE, GITHUB_ACTOR } = process.env;
+const { GITHUB_SHA, GITHUB_EVENT_PATH, GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_WORKSPACE, GITHUB_ACTOR } = process.env;
 
 // action file information: https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/metadata-syntax-for-github-actions
 async function main() {
   try {
-
-    console.log(GITHUB_SHA)
-    console.log(GITHUB_EVENT_PATH)
-    console.log(GITHUB_TOKEN)
-    console.log(GITHUB_WORKSPACE)
-    console.log(GITHUB_ACTOR)
-
-
-    const github_token = core.getInput("GITHUB_TOKEN", { required: true })
+    const github_token = GITHUB_TOKEN//core.getInput("GITHUB_TOKEN", { required: true })
     console.log(github_token);
-    const repo = core.getInput("GITHUB_REPOSITORY", { required: true }) //github.context.repository;
+    const repo = GITHUB_REPOSITORY;//core.getInput("GITHUB_REPOSITORY", { required: true }) //github.context.repository;
     console.log(repo)
-    const owner = core.getInput("GITHUB_ACTOR", { required: true }) //github.context.repository_owner;
+    const owner = GITHUB_ACTOR //core.getInput("GITHUB_ACTOR", { required: true }) //github.context.repository_owner;
     console.log(owner)
 
 
