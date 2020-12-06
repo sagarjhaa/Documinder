d1480a33e397c30780389ccdfec121956d76bb50
Sagar <sagar.jhaa@gmail.com>
2020-12-06 01:04:37 -0800

    read pull request files

--- src/index.js
+++ src/index.js
@@ -5,15 +5,26 @@
 // slack message to intimate if we need an update
 // mail if we need to alert
 
-const core = require('@actions/core')
+// const core = require('@actions/core');
+const github = require('@actions/github');
 
 // action file information: https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/metadata-syntax-for-github-actions
-function main() {
+async function main() {
   try {
-    const pr = core.getInput("pr", { required: true })
-    const rules = core.getInput("rules", { required: true })
 
-    console.log(`received pr - ${pr} and rules - ${rules}`)
+    //const token = core.getInput("GITHUB_TOKEN", { required: true })
+    const context = github.context;
+    const owner = github.repository_owner;
+    const repo = github.repository;
+    const pull_request_number = context.payload.pull_request.number;
+
+    const pull_request_files = await octokit.pulls.listFiles({
+      owner,
+      repo,
+      pull_request_number,
+    });
+
+    console.log(pull_request_files);
 
   } catch (error) {
     console.log(`error occured ${error}`);
