1c9563d8ed8ace0eebe266a3dc5bbc67b0f878a1
Sagar <sagar.jhaa@gmail.com>
2020-12-05 21:18:11 -0800

    document is live

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@
 // compare the files
 // slack message to intimate if we need an update
 // mail if we need to alert
+
+
+// action file information: https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/metadata-syntax-for-github-actions
+function main() {
+  console.log("documinder is live");
+}
+
+main();
